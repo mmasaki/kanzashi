473f1b1a75626aebbd95f5f768890425754fd0bc
Glass_saga <glass.saga@gmail.com>
2011-07-23 11:03:18 +0900

    binにkanzashi.rbを追加

---
+++ bin/kanzashi.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+require './lib/kanzashi'
+
+EventMachine::run do
+  Kanzashi::Server.start_and_connect("config.yml")
+  EventMachine::start_server "0.0.0.0", 8082, Kanzashi::Server
+end
