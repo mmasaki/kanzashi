1ddfaae0f54d756ee313aecf8241c43af7595414
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-07-25 20:59:59 +0900

    Kanzashi::Config

--- bin/kanzashi.rb
+++ bin/kanzashi.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require_relative '../lib/kanzashi'
 
+Kanzashi::Config.parse(ARGV)
 EventMachine::run do
-  Kanzashi::Server.parse(ARGV)
   Kanzashi::Server.start_and_connect
   EventMachine::start_server "0.0.0.0", 8082, Kanzashi::Server
 end
--- lib/kanzashi.rb
+++ lib/kanzashi.rb
@@ -7,7 +7,6 @@ require 'digest/sha2'
 
 module Kanzashi
   DEBUG = true # a flag to enable/disable debug print
-  @@config = nil
 
   module Util
     class CustomHash < Hash
@@ -49,14 +48,77 @@ module Kanzashi
         end
       end
     end
+
   end
 
-  @@config = {}
+  module UtilMethod
+    def config
+      Config.config
+    end
+  end
+  include UtilMethod
+  class << self; include UtilMethod; end
+
+  module Config
+    include Kanzashi
+    @@config = {
+      config_file: "config.yml",
+      user: {
+        nick: "kanzashi",
+        user: "kanzashi",
+        real: "kanzashi that better tiarra"
+      },
+      server: {
+        port: 8081,
+        bind: "0.0.0.0",
+        pass: nil,
+        tls: false
+      },
+      networks: {}
+    }
+    @@old_config = nil
+    @@config = Util::CustomHash.new(@@config)
+
+    class << self
+      def load_config
+        @@old_config = @@config.dup
+        yaml = Util::CustomHash.new(YAML.load(open(@@config[:config_file])))
+        yaml.delete :config_file
+        [:user,:server].each do |k|
+          if (_ = yaml.delete(k))
+            @@config[k].merge! _
+          end
+        end
+        @@config.merge! yaml
+        @@config = Util::CustomHash.new(@@config)
+      end
+
+      def config
+        @@old_config ? @@config : load_config
+      end
+
+      def parse(argv)
+        parser = OptionParser.new
+        config_file = "config.yml"
+
+        parser.on('-c FILE','--config=FILE','specify config file') do |file|
+          @@config[:config_file] = file
+        end
+
+        parser.parse(argv)
+
+        load_config
+        self
+      end
+    end
+  end
 
   # debug print
+  # TODO: This should be replaced by Logger. (by sorah)
   def debug_p(str)
     p str if DEBUG
   end
+
 
   # a module to communicate with IRC servers as a client
   module Client
@@ -135,7 +197,7 @@ module Kanzashi
     def join(channel_name)
       channel_sym = channel_name.to_sym
       if @channels.keys.include?(channel_sym)
-        relay(":#{@@config[:nick]} JOIN :#{channel_name}@#{@server_name}\r\n")
+        relay(":#{config[:nick]} JOIN :#{channel_name}@#{@server_name}\r\n")
         @channels[channel_sym].each do |line|
           relay(line)
         end
@@ -155,6 +217,7 @@ module Kanzashi
   # a module behaves like an IRC server to IRC clients.
   module Server
     include Kanzashi
+    class << self; include UtilMethod; end
 
     def initialize
       Client.add_connection(self)
@@ -162,57 +225,18 @@ module Kanzashi
     end
 
     def post_init
-      if @@config[:server][:tls] # enable TLS
-        start_tls(@@config[:server][:tls].kind_of?(Hash) ? @@config[:server][:tls] : {})
+      if config[:server][:tls] # enable TLS
+        start_tls(config[:server][:tls].kind_of?(Hash) ? config[:server][:tls] : {})
       end
-    end
-
-    def self.load_config(filename)
-      @@config = {
-        user: {
-          nick: "kanzashi",
-          user: "kanzashi",
-          real: "kanzashi that better tiarra"
-        },
-        server: {
-          port: 8081,
-          bind: "0.0.0.0",
-          pass: nil,
-          tls: false
-        },
-        networks: {}
-      }
-      yaml =  Util::CustomHash.new(YAML.load(open(filename)))
-      [:user,:server].each do |k|
-        if (_ = yaml.delete(k))
-          @@config[k].merge! _
-        end
-      end
-      @@config.merge! yaml
-      @@config = Util::CustomHash.new(@@config)
-    end
-
-    def self.parse(argv)
-      parser = OptionParser.new
-      config_file = "config.yml"
-
-      parser.on('-c FILE','--config=FILE','specify config file') do |file|
-        config_file = file
-      end
-
-      parser.parse(argv)
-
-      load_config(config_file)
-      self
     end
 
     def self.start_and_connect
       @@servers = {}
       # connect to specified server
-      @@config[:networks].each do |server_name, value|
+      config[:networks].each do |server_name, value|
         connection = EventMachine::connect(value[:host], value[:port], Client, server_name, value[:encoding], value[:tls])
         @@servers[server_name] = connection
-        connection.send_data("NICK #{@@config[:user][:nick]}\r\nUSER #{@@config[:user][:user]||@@config[:user][:nick]} 8 * :#{@@config[:user][:real]}\r\n")
+        connection.send_data("NICK #{config[:user][:nick]}\r\nUSER #{config[:user][:user]||config[:user][:nick]} 8 * :#{config[:user][:real]}\r\n")
       end
     end
 
@@ -220,9 +244,9 @@ module Kanzashi
       m = Net::IRC::Message.parse(line)
       p m
       if  m.command ==  "PASS"
-        p @@config[:server][:pass]
+        p config[:server][:pass]
         p m[0].to_s
-        @auth = (@@config[:server][:pass] == Digest::SHA256.hexdigest(m[0].to_s) || @@config[:server][:pass] == m[0].to_s)
+        @auth = (config[:server][:pass] == Digest::SHA256.hexdigest(m[0].to_s) || config[:server][:pass] == m[0].to_s)
         p @auth
       end
       close_connection unless @auth
