06b4b8987e81344ebfba8dcd8908aa3ce8ff715c
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-08-02 11:35:59 +0900

    refactoring

--- bin/kanzashi.rb
+++ bin/kanzashi.rb
@@ -2,7 +2,7 @@
 require_relative '../lib/kanzashi'
 
 Kanzashi::Config.parse(ARGV)
-EventMachine::run do
+EventMachine.run do
   Kanzashi::Server.start_and_connect
-  EventMachine::start_server Kanzashi.config.server.bind, Kanzashi.config.server.port, Kanzashi::Server
+  EventMachine.start_server Kanzashi.config.server.bind, Kanzashi.config.server.port, Kanzashi::Server
 end
