881f543377f2f6baaff6a51f81ee25e61892e42a
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-08-03 16:53:07 +0900

    use attached hook

--- plugins/detached_away.rb
+++ plugins/detached_away.rb
@@ -1,11 +1,7 @@
 Kh.detached do
   K::Server.send_to_all("AWAY :#{K.config.plugins.detatched_away.message}\r\n")
-  @detached = true
 end
 
-Kh.new_session do
-  if @detached
-    K::Server.send_to_all("AWAY\r\n")
-    @detached = false
-  end
+Kh.attached do
+  K::Server.send_to_all("AWAY\r\n")
 end
--- plugins/detached_nick.rb
+++ plugins/detached_nick.rb
@@ -1,11 +1,7 @@
 Kh.detached do
   K::Server.send_to_all("NICK #{K.config.plugins.detatched_nick.nick_on_detached}\r\n")
-  @detached = true
 end
 
-Kh.new_session do
-  if @detached
-    K::Server.send_to_all("NICK #{K.config.user.nick}\r\n")
-    @detached = false
-  end
+Kh.attached do
+  K::Server.send_to_all("NICK #{K.config.user.nick}\r\n")
 end
