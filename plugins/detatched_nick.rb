be3f52620b29181e2dcf5ba50ea90dbdb791fdc2
Glass_saga <glass.saga@gmail.com>
2011-08-03 16:37:18 +0900

    fix bug of detatched_nick.rb

--- plugins/detatched_nick.rb
+++ plugins/detatched_nick.rb
@@ -1,5 +1,5 @@
 Kh.detached do
-  K::Server.send_to_all("NICK #{K.config.plugins.user_nick_detached.nick_on_detached}\r\n")
+  K::Server.send_to_all("NICK #{K.config.plugins.detatched_nick.nick_on_detached}\r\n")
   @detached = true
 end
 
