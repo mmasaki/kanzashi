6ba83140c88e2bce16c7be18445fd584f6aae347
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-08-03 16:54:28 +0900

    typo

--- plugins/detached_nick.rb
+++ plugins/detached_nick.rb
@@ -1,5 +1,5 @@
 Kh.detached do
-  K::Server.send_to_all("NICK #{K.config.plugins.detatched_nick.nick_on_detached}\r\n")
+  K::Server.send_to_all("NICK #{K.config.plugins.detached_nick.nick_on_detached}\r\n")
 end
 
 Kh.attached do
