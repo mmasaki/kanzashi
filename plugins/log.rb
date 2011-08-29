29188de7f987fa40f914e69202058a695ff2d67d
Glass_saga <glass.saga@gmail.com>
2011-08-30 01:19:53 +0900

    config in log.rb

--- plugins/log.rb
+++ plugins/log.rb
@@ -1,49 +1,55 @@
 #encoding: utf-8
 
 class Kanzashi::Plugin::Log
+  DEBUG = true
+
   def initialize
     @directory = K.c[:directory] || "log"
     @header = K.c[:header] || "%T"
     @filename = K.c[:filename] || "%Y.%m.%d.txt" 
+    @mode = K.c[:mode] || 0600
+    @dir_mode = K.c[:dir_mode] || 0700
     @persistent = K.c[:persistent]
+    @logfiles = {} if @persistent
 
-    Dir.mkdir(@directory) unless File.directory?(@directory)
+    Dir.mkdir(@directory, @dir_mode) unless File.directory?(@directory)
   end  
 
   attr_reader :persistent
 
-  def self.puts(str, dst)
-    str = "#{Time.now.strftime(K.c[:header])} #{str}"
-    STDOUT.puts(str)
+  def puts(str, dst)
+    str.replace("#{Time.now.strftime(K.c[:header])} #{str}")
+    STDOUT.puts(str) if DEBUG
     if @persistent
-      @logfiles[server_name][channel].puts(str)
+      @logfiles[dst.to_sym].puts(str)
     else
-      path = "log/#{dst}/#{Time.now.strftime(K.c[:filename])}"
-      File.open(path, "a", ) { |f| f.puts(str) }
+      path = "#{@directory}/#{dst}/#{Time.now.strftime(@filename)}"
+      File.open(path, "a", @mode) { |f| f.puts(str) }
     end
   end
 
-  def file_open(dir)
-    File.open("#{dir}/#{Time.now.strftime("%Y-%m-%d")}", "a")
+  def file_open(dst)
+    dst.replace("#{@directory}/#{dst}")
+    Dir.mkdir(dst, @dir_mode) unless File.directory?(dst)
+    File.open("#{dst}/#{Time.now.strftime(@filename)}", "a", @mode)
+  end
+
+  def add_dst(channel_name)
+    @logfiles[channel_name.to_sym] = self.file_open(channel_name)
   end
 end
 
 Kh.start do
   @log = Kanzashi::Plugin::Log.new
-  @logfiles = {} if @log.persistent
 end
 
 Kh.join do |m, module_|
   if module_.kind_of?(K::Client)
     nick, = K::UtilMethod.parse_prefix(m.prefix)
-    if nick == module_.nick # Kanzashi's join
-      dir = "log/#{m[0]}@#{module_.server_name}"
-      Dir.mkdir(dir) unless File.directory?(dir)
-      if @log.persistent 
-        @logfiles[module_.server_name.to_sym] = { m[0].to_s.to_sym => K::Plugin::Log.file_open(dir) }
-      end
+    if nick == module_.nick && @log.persistent # Kanzashi's join
+      @log.add_dst("#{m[0].to_s}@#{module_.server_name}")
     else # others join
-      K::Plugin::Log.puts("+ #{nick} (#{m.prefix}) to #{m[0]}@#{module_.server_name}", "#{m[0]}@#{module_.server_name}")
+      @log.puts("+ #{nick} (#{m.prefix}) to #{m[0]}@#{module_.server_name}", "#{m[0]}@#{module_.server_name}")
     end
   end
 end
@@ -51,32 +57,31 @@ end
 Kh.part do |m, module_|
   if module_.kind_of?(K::Client)
     nick, = K::UtilMethod.parse_prefix(m.prefix)
-    K::Plugin::Log.puts("- #{nick} (\"#{m[1]}\")", "#{m[0]}@#{module_.server_name}")
+    @log.puts("- #{nick} (\"#{m[1]}\")", "#{m[0]}@#{module_.server_name}")
   end
 end
 
 Kh.kick do |m, module_|
   if module_.kind_of?(K::Client)
     nick, = K::UtilMethod.parse_prefix(m.prefix)
-    K::Plugin::Log.puts("- #{m[1]} by #{nick} from#{m[0]}@#{module_.server_name} (#{m[2]})", "#{m[0]}@#{module_.server_name}")
+    @log.puts("- #{m[1]} by #{nick} from#{m[0]}@#{module_.server_name} (#{m[2]})", "#{m[0]}@#{module_.server_name}")
   end
 end
 
 Kh.mode do |m, module_|
   if module_.kind_of?(K::Client) && /^(#|&).+$/ =~ m[0].to_s # to avoid usermode MODE messages
     nick, = K::UtilMethod.parse_prefix(m.prefix)
-    K::Plugin::Log.puts("Mode by #{nick}: #{m[0]} #{m[1]} #{m[2]}", "#{m[0]}@#{module_.server_name}")
+    @log.puts("Mode by #{nick}: #{m[0]} #{m[1]} #{m[2]}", "#{m[0]}@#{module_.server_name}")
   end
 end
 
 Kh.privmsg do |m, module_|
-  p K.c
   unless m.ctcp?
     nick, = K::UtilMethod.parse_prefix(m.prefix)
     if module_.kind_of?(K::Client) # from others
-      K::Plugin::Log.puts("<#{m[0]}:#{nick}> #{m[1]}", "#{m[0]}@#{module_.server_name}")
+      @log.puts("<#{m[0]}:#{nick}> #{m[1]}", "#{m[0]}@#{module_.server_name}")
     else # from Kanzashi's client
-      K::Plugin::Log.puts(">#{m[0]}:#{module_.user[:nick]}< #{m[1]}", m[0].to_s)
+      @log.puts(">#{m[0]}:#{module_.user[:nick]}< #{m[1]}", m[0].to_s)
     end
   end
 end
@@ -85,9 +90,9 @@ Kh.notice do |m, module_|
   unless m.ctcp?
     nick, = K::UtilMethod.parse_prefix(m.prefix)
     if module_.kind_of?(K::Client) # from others
-      K::Plugin::Log.puts("(#{m[0]}:#{nick}) #{m[1]}", "#{m[0]}@#{module_.server_name}") unless m[0] == "*"
+      @log.puts("(#{m[0]}:#{nick}) #{m[1]}", "#{m[0]}@#{module_.server_name}") unless m[0] == "*"
     else # from Kanzashi's client
-      K::Plugin::Log.puts(")#{m[0]}:#{module_.user[:nick]}( #{m[1]}", m[0].to_s)
+      @log.puts(")#{m[0]}:#{module_.user[:nick]}( #{m[1]}", m[0].to_s)
     end
   end
 end
@@ -96,7 +101,7 @@ Kh.nick do |m, module_|
   if module_.kind_of?(K::Client)
     nick, = K::UtilMethod.parse_prefix(m.prefix)
     module_.channels.each do |channel, value|
-      K::Plugin::Log.puts("#{nick} -> #{m[0]}", "#{channel}@#{module_.server_name}") if value[:names].include?(nick)
+      @log.puts("#{nick} -> #{m[0]}", "#{channel}@#{module_.server_name}") if value[:names].include?(nick)
     end
   end
 end
