6217e3f8df5e67fc41d540f3c04fdf17e1668db1
Glass_saga <glass.saga@gmail.com>
2013-01-02 17:01:43 +0900

    detached nick

--- plugins/detached_nick.rb
+++ plugins/detached_nick.rb
@@ -1,12 +1,20 @@
-Kh.detached do
-  nick_on_detached = K.config.plugins.detached_nick.nick_on_detached
-  if nick_on_detached && nick_on_detached.kind_of?(String)
-    K::Server.networks.each do |name,client|
-      client.nick = K.config.plugins.detached_nick.nick_on_detached
+nick_on_detached = K.config.plugins.detached_nick.nick_on_detached
+origin_nick = K.config.user.nick
+
+if nick_on_detached && nick_on_detached.kind_of?(String)
+  Kanzashi.plugin do
+    from :Server do |server_mod|
+      on :detached do
+        server_mod.networks.each do |name,client|
+          client.nick = nick_on_detached
+        end
+      end
+  
+      on :attached do
+        server_mod.networks.each do |name,client|
+          client.nick = origin_nick
+        end
+      end
     end
   end
-end
-
-Kh.attached do
-  K::Server.send_to_all("NICK #{K.config.user.nick}\r\n")
 end
